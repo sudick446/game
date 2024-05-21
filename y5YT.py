fd06f5f08459a947042dd6d4b0d6a8e527ebb25b
sudick446 <mutsinzisudick446@gmail.com>
2024-05-21 09:19:52 +0200

    Update y5YT.py

--- y5YT.py
+++ y5YT.py
@@ -1,6 +1,6 @@
 import random
 
-print('Start \'rock-paper-scissors\' game')
+print('Start \'rock-paper-scissors\'')
 
 print('Input your hand choice')
 your_hand = int(input('0:rock, 1:scissors, 2:paper'))
@@ -8,11 +8,22 @@ computer_hand = random.randint(0, 2)
 
 if your_hand == 0:
     if computer_hand == 0:
-        print('Draw')
+        print('draw')
     elif computer_hand == 1:
-        print('Win')
+        print('win')
+    elif computer_hand == 2:
+        print('lose')
 elif your_hand == 1:
     if computer_hand == 0:
-        print('Lose')
+        print('lose')
     elif computer_hand == 1:
-        print('Draw')
+        print('draw')
+    elif computer_hand == 2:
+        print('win')
+elif your_hand == 2:
+    if computer_hand == 0:
+        print('win')
+    elif computer_hand == 1:
+        print('lose')
+    elif computer_hand == 2:
+        print('draw')
