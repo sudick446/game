bfe5f4d2b565db5729b7f87f3b3ff7c7e52693dd
sudick446 <mutsinzisudick446@gmail.com>
2024-05-21 16:06:42 +0200

    Update a5Qm.py

--- a5Qm.py
+++ a5Qm.py
@@ -1,16 +1,32 @@
 import random
 
-print('Start \'rock-paper-scissors\'')
 
-print('Input your hand')
-your_hand = int(input('0:rock, 1:scissors, 2:paper'))
-computer_hand = random.randint(0, 2)
+def start_message():
+    print('Start \'rock-paper-scissors\'')
 
+
+def get_player():
+    print('Input your hand')
+    return int(input('0:rock, 1:scissors, 2:paper'))
+
+
+def get_computer():
+    return random.randint(0, 2)
+
+
+def view_result(hand_diff):
+    if hand_diff == 0:
+        print('draw')
+    elif hand_diff == 1 or hand_diff == 2:
+        print('win')
+    else:
+        print('lose')
+
+
+start_message()
+
+your_hand = get_player()
+computer_hand = get_computer()
 hand_diff = your_hand - computer_hand
 
-if hand_diff == 0:
-    print('draw')
-elif hand_diff == -1 or hand_diff == 2:
-    print('win')
-else:
-    print('lose')
+view_result(hand_diff)
